723ced5b69b04af02c9a4e0c70bec1aa238013e2
Erikos <Erikos66@outlook.com>
2025-03-14 14:02:41 +0000

    Add meta files for new assets including fruits, player, and tiles

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance;
+
+    public Player player;
+
+    public int fruitsCollected;
+
+    private void Awake() {
+        if (instance == null) {
+            instance = this;
+        }
+        else if (instance != this) {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Score() {
+        Debug.Log("Score!");
+    }
+
+    public void AddFruit() {
+        fruitsCollected++;
+        Debug.Log("Fruit collected! Total: " + fruitsCollected);
+    }
+
+}
