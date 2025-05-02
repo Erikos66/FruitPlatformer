6c72f49d8d827a43e22f50fb1dffb795abe6b426
Erikos <Erikos66@outlook.com>
2025-05-02 16:27:11 +0100

    Update background material offset and refine player prefab parameters

--- Assets/Scripts/Levels/EndPoint.cs
+++ Assets/Scripts/Levels/EndPoint.cs
@@ -2,26 +2,28 @@ using UnityEngine;
 
 public class EndPoint : MonoBehaviour {
 
-    private Animator anim;
+	private Animator anim;
 
-    void Awake() {
-        anim = GetComponent<Animator>();
-    }
+	void Awake() {
+		anim = GetComponent<Animator>();
+	}
 
-    private void OnTriggerEnter2D(Collider2D collision) {
-        Player player = collision.GetComponent<Player>();
-        if (player) {
-            // Play level finish sound
-            AudioManager.Instance.PlaySFX("SFX_Finish");
+	private void OnTriggerEnter2D(Collider2D collision) {
+		Player player = collision.GetComponent<Player>();
+		if (player) {
+			// Play level finish sound
+			AudioManager.Instance.PlaySFX("SFX_Finish");
 
-            anim.SetTrigger("activate");
-            Debug.Log("Level complete!");
+			anim.SetTrigger("activate");
+			Debug.Log("Level complete!");
 
-            // Explicitly stop the timer when player reaches the end point
-            TimerManager.Instance.StopLevelTimer();
+			// Explicitly stop the timer when player reaches the end point
+			TimerManager.Instance.StopLevelTimer();
 
-            // Mark the level as finished and handle progression
-            LevelManager.Instance.LevelFinished();
-        }
-    }
+			// Mark the level as finished and handle progression
+			LevelManager.Instance.LevelFinished();
+
+			player.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+		}
+	}
 }
