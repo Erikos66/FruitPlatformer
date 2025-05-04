a7a770c0396bfe0d910d1ec6a083f010c945b8eb
Erikos <Erikos66@outlook.com>
2025-05-04 23:30:39 +0100

    Update material offsets, player prefab collider settings, and main menu scene properties; enhance skin selector functionality

--- Assets/Scripts/UI/UI_SkinSelector.cs
+++ Assets/Scripts/UI/UI_SkinSelector.cs
@@ -3,58 +3,61 @@ using UnityEngine.EventSystems;
 
 public class UI_SkinSelector : MonoBehaviour {
 
-    [SerializeField] private Animator chrSkinDisplayAnimator;
-    [SerializeField] private int selectedSkinIndex = 0;
-    [SerializeField] private GameObject firstSelectedButton;
+	[SerializeField] private Animator chrSkinDisplayAnimator;
+	[SerializeField] private GameObject charSkinDisplayObject;
+	[SerializeField] private int selectedSkinIndex = 0;
+	[SerializeField] private GameObject firstSelectedButton;
 
 
-    void OnEnable() {
-        EventSystem.current.SetSelectedGameObject(firstSelectedButton);
-    }
+	void OnEnable() {
+		EventSystem.current.SetSelectedGameObject(firstSelectedButton);
+		charSkinDisplayObject.SetActive(true);
+		for (int i = 0; i < chrSkinDisplayAnimator.layerCount; i++) {
+			chrSkinDisplayAnimator.SetLayerWeight(i, 0f);
+		}
+	}
 
-    public void NextSkin() {
-        selectedSkinIndex++;
-        if (selectedSkinIndex > 3) {
-            selectedSkinIndex = 0;
-        }
-        AudioManager.Instance.PlayRandomSFX("SFX_MenuSelect");
-        UpdateSkin();
-    }
+	void OnDisable() {
+		charSkinDisplayObject.SetActive(false);
+	}
 
-    public void PreviousSkin() {
-        selectedSkinIndex--;
-        if (selectedSkinIndex < 0) {
-            selectedSkinIndex = 3;
-        }
-        AudioManager.Instance.PlayRandomSFX("SFX_MenuSelect");
-        UpdateSkin();
-    }
+	public void NextSkin() {
+		selectedSkinIndex++;
+		if (selectedSkinIndex > 3) {
+			selectedSkinIndex = 0;
+		}
+		AudioManager.Instance.PlayRandomSFX("SFX_MenuSelect");
+		UpdateSkin();
+	}
+
+	public void PreviousSkin() {
+		selectedSkinIndex--;
+		if (selectedSkinIndex < 0) {
+			selectedSkinIndex = 3;
+		}
+		AudioManager.Instance.PlayRandomSFX("SFX_MenuSelect");
+		UpdateSkin();
+	}
 
 
-    private void UpdateSkin() {
-        for (int i = 0; i < chrSkinDisplayAnimator.layerCount; i++) {
-            if (i == selectedSkinIndex) {
-                chrSkinDisplayAnimator.SetLayerWeight(i, 1f);
-            }
-            else {
-                chrSkinDisplayAnimator.SetLayerWeight(i, 0f);
-            }
-        }
-    }
+	private void UpdateSkin() {
+		for (int i = 0; i < chrSkinDisplayAnimator.layerCount; i++) {
+			if (i == selectedSkinIndex) {
+				chrSkinDisplayAnimator.SetLayerWeight(i, 1f);
+			}
+			else {
+				chrSkinDisplayAnimator.SetLayerWeight(i, 0f);
+			}
+		}
+	}
 
-    void Awake() {
-        for (int i = 0; i < chrSkinDisplayAnimator.layerCount; i++) {
-            chrSkinDisplayAnimator.SetLayerWeight(i, 0f);
-        }
-    }
-
-    public void SelectSkin() {
-        if (GameManager.Instance != null && PlayerManager.Instance != null) {
-            SkinManager.Instance.SetSkin(selectedSkinIndex);
-            Debug.Log("Selected skin index: " + selectedSkinIndex);
-        }
-        else {
-            Debug.LogError("Cannot select skin: GameManager or playerManager is not initialized.");
-        }
-    }
+	public void SelectSkin() {
+		if (GameManager.Instance != null && PlayerManager.Instance != null) {
+			SkinManager.Instance.SetSkin(selectedSkinIndex);
+			Debug.Log("Selected skin index: " + selectedSkinIndex);
+		}
+		else {
+			Debug.LogError("Cannot select skin: GameManager or playerManager is not initialized.");
+		}
+	}
 }
